71a012c4b2360fc45473d18ed220777b8fee0c7e
devqueue <haziq.sayyed@gmail.com>
2022-02-02 20:33:19 +0530

    Renamed year column

--- dashboard/models.py
+++ dashboard/models.py
@@ -15,6 +15,6 @@ class Utilization(models.Model):
     October = models.FloatField()
     November = models.FloatField()
     December = models.FloatField()
-    year = models.IntegerField()
+    Year = models.IntegerField()
     MonthlyIndex = models.FloatField()
 
