11bc9b86c970d1ca52b3a8567f6a2b72e989d926
devqueue <haziq.sayyed@gmail.com>
2022-02-05 11:59:39 +0530

    new model and serializer added

--- dashboard/serializers.py
+++ dashboard/serializers.py
@@ -1,9 +1,25 @@
 from rest_framework import serializers
 from dashboard.models import Utilization
+from dashboard.models import Processed
+from dashboard.models import Revenue
+
+
 
 class UtilizationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Utilization
+        fields = '__all__'
+
+
+class UtilizationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Processed
+        fields = '__all__'
+
+
+class UtilizationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Processed
         fields = '__all__'
 
 
