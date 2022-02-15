700549cdc8b561fb85bd2e6c7b7c7f2d48d87732
Haziq sayyed <Haziq.sayyed@gmail.com>
2022-02-15 22:48:19 +0530

    urls for upload app added

--- djangobackend/urls.py
+++ djangobackend/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
+    path('', include('upload.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('authenticate.urls')),
 ]
