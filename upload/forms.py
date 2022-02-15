e6bbab5659abb858d019f873bdf5bbe91ff92753
Haziq sayyed <Haziq.sayyed@gmail.com>
2022-02-15 22:48:30 +0530

    upload form added

---
+++ upload/forms.py
@@ -0,0 +1,8 @@
+from dataclasses import fields
+from django import forms
+from .models import Csv
+
+class CsvModelForm(forms.ModelForm):
+    class Meta:
+        model = Csv
+        fields = ('file_name',)
