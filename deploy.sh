b9d4072a7a6ad1d73f31df152dcf756dc6dc0911
devqueue <haziq.sayyed@gmail.com>
2022-02-25 09:56:14 +0530

    more steps added

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+apt-get install python3-dev default-libmysqlclient-dev build-essential
 python3 -m pip install -r requirements.txt
+
+chmod 0444 mysql.cnf
+
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py createsuperuser
